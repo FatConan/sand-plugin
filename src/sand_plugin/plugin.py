eaaa35a497c7310833356e3462b578078ce134fe
Fatconan <ian@headwillcollapse.net>
2021-09-18 23:13:59 +0100

    Initial checkin

---
+++ src/sand_plugin/plugin.py
@@ -0,0 +1,10 @@
+class SandPlugin:
+    def configure(self, site_data, site):
+        pass
+
+    def parse(self, site_data, site):
+        pass
+
+    def add_render_context(self, page, environment, data):
+        pass
+
